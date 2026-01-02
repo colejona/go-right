1cdd54bd5460163cb7558a3263fbcc1178c63fdb
colejona <colejona@gmail.com>
2026-01-02 04:01:46 -0800

    Replace Game/GameOver with Field

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,9 +1,8 @@
 import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
+import {Field} from "./scenes/Field";
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
@@ -17,8 +16,7 @@ const config: Phaser.Types.Core.GameConfig = {
         Boot,
         Preloader,
         MainMenu,
-        MainGame,
-        GameOver
+        Field,
     ]
 };
 
---
+++ src/game/scenes/Field.ts
@@ -0,0 +1,20 @@
+import {Scene} from "phaser";
+
+export class Field extends Scene {
+    // TODO: is this needed?
+    constructor() {
+        super('Field');
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
